b602a3948611fa49a9de539139d12d2fc6b8755c
ryanPfaffman <73614186+ryanPfaffman@users.noreply.github.com>
2020-12-15 21:15:06 -0500

    Update find_int.cs

--- find_int.cs
+++ find_int.cs
@@ -1,3 +1,11 @@
+using System;
+using System.Linq;
+/*
+Given an array of integers, find the one that appears an odd number of times.
+
+There will always be only one integer that appears an odd number of times.
+*/
+
 public static int find_it(int[] seq)
         {
             var groups = seq.GroupBy(x => x);
